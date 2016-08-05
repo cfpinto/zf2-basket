431f5dc4a5c2a85ae27897a4cb2fbf9833f530ee
Claudio Pinto <claudio@payasugym.com>
2016-08-05 17:47:37 +0100

    Cookie adapter + container method

---
+++ src/Zf2Basket/Product/ProductItem.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cfpinto
+ * Date: 05/08/2016
+ * Time: 16:33
+ */
+
+namespace Zf2Basket\Product;
+
+
+class ProductItem extends AbstractProduct
+{
+
+}
