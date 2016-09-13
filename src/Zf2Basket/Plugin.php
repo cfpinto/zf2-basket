9917dbcb87ecbc9585a54a61a083e034200a8c9b
Claudio Pinto <claudiopinto@Claudios-iMac.local>
2016-09-13 09:24:27 +0100

    PAUG-2288

---
+++ src/Zf2Basket/Plugin.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: claudiopinto
+ * Date: 13/09/2016
+ * Time: 09:19
+ */
+
+namespace Zf2Basket;
+
+
+class Plugin
+{
+
+}
---
+++ src/Zf2Basket/ViewHelper.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: claudiopinto
+ * Date: 13/09/2016
+ * Time: 09:19
+ */
+
+namespace Zf2Basket;
+
+use Zend\View\Helper\AbstractHelper;
+
+class ViewHelper extends AbstractHelper
+{
+    /**
+     * @var Zf2Basket
+     */
+    private $basket;
+
+    function __construct(Zf2Basket $basket)
+    {
+        $this->basket = $basket;
+    }
+
+    function __invoke()
+    {
+        return $this->basket;
+    }
+
+    function __call($name, $arguments)
+    {
+        return call_user_func_array([$this->auth, $name], $arguments);
+    }
+}
